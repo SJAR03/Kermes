602f7bcf2d334ce00b94f941a259d7b658c87b6f
Sergio Ayerdis <sayerdisrodriguez@gmail.com>
2021-11-22 00:43:59 -0600

    CRUD tabla rol

--- pages/Catalogos/tbl_rol.php
+++ pages/Catalogos/tbl_rol.php
@@ -29,7 +29,9 @@ if (isset($varMsj)) {
     <link rel="stylesheet" href="../../plugins/DT/Responsive-2.2.9/css/responsive.bootstrap.min.css">
     <link rel="stylesheet" href="../../plugins/DT/Buttons-2.0.0/css/buttons.bootstrap4.min.css">
     <!-- Theme style -->
+    <link rel="stylesheet" href="../../plugins/jAlert/dist/jAlert.css">
     <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+
 </head>
 
 <body class="hold-transition sidebar-mini">
@@ -479,7 +481,6 @@ if (isset($varMsj)) {
                                     <tr>
                                         <th>ID Rol</th>
                                         <th>Rol Descripción</th>
-                                        <th>Estado</th>
                                         <th>Opciones</th>
                                     </tr>
                                 </thead>
@@ -490,10 +491,10 @@ if (isset($varMsj)) {
                                         <tr>
                                             <td><?php echo $r->__GET('id_rol'); ?></td>
                                             <td><?php echo $r->__GET('rol_descripcion'); ?></td>
-                                            <td><?php echo $r->__GET('estado'); ?></td>
                                             <td>
                                                 <a href="frm_edit_rol.php?editRol=<?php echo $r->__GET('id_rol') ?>"><i class="far fa-edit fa-2x" title="Editar rol"></i></a>
                                                 <a href="frm_view_rol.php?vieRol=<?php echo $r->__GET('id_rol') ?>"><i class="far fa-eye fa-2x" title="Visualizar rol"></i></a>
+                                                <a href="#" onclick="deleteRol(<?php echo $r->__GET('id_rol') ?>);"><i class="far fa-2x fa-trash-alt" title="Eliminar rol"></i></a>
                                             </td>
                                         </tr>
                                     <?php
@@ -504,7 +505,6 @@ if (isset($varMsj)) {
                                     <tr>
                                         <th>ID Rol</th>
                                         <th>Rol Descripción</th>
-                                        <th>Estado</th>
                                         <th>Opciones</th>
                                     </tr>
                                 </tfoot>
@@ -548,6 +548,11 @@ if (isset($varMsj)) {
         <script src="../../plugins/DT/Buttons-2.0.0/js/buttons.print.min.js"></script>
         <script src="../../plugins/DT/Buttons-2.0.0/js/buttons.colVis.min.js"></script>
 
+        <!-- JAlert js -->
+        <script src="../../plugins/jAlert/dist/jAlert.min.js"></script>
+        <script src="../../plugins/jAlert/dist/jAlert-functions.min.js">
+
+        </script>
 
         <!-- AdminLTE App -->
         <script src="../../dist/js/adminlte.min.js"></script>
@@ -555,6 +560,39 @@ if (isset($varMsj)) {
         <script src="../../dist/js/demo.js"></script>
         <!-- Page specific script -->
         <script>
+            function deleteRol(idR) {
+                confirm(function(e, btn) {
+                        e.preventDefault();
+                        window.location.href = "../../negocio/ng_rol.php?delRol=" + idR;
+                    },
+                    function(e, btn) {
+                        e.preventDefault();
+                    });
+            }
+
+            $(document).ready(function() {
+                // Mensajes de Control
+                var mensaje = 0;
+                mensaje = "<?php echo $varMsj ?>";
+
+                switch (mensaje) {
+                    case "1":
+                        successAlert('Éxito', 'Se han registrado exitosamente los datos.');
+                        break;
+
+                    case "2":
+                    case "4":
+                        errorAlert('Fallo', 'Revise los datos. Intente de nuevo.');
+                        break;
+
+                    case "3":
+                        successAlert('Éxito', 'Se modificó exitosamente el rol.');
+                    default:
+
+                }
+
+            });
+
             $(function() {
                 $("#example1").DataTable({
                     "responsive": true,
